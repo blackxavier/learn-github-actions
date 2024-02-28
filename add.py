d56b8be7b3a8329db0911456d79a8178e7850828
Ogbuchi Arinze Valentine <56504681+blackxavier@users.noreply.github.com>
2024-02-28 20:47:17 +0100

    Update add.py

--- add.py
+++ add.py
@@ -3,3 +3,10 @@ second_number  = 3
 
 total = first_number + second_number
 print(total)
+
+def multiple(x,y):
+  return x*y
+
+def test_add():
+  assert multiple(2,3) == 6
+  assert multiple(4,4) == 16
